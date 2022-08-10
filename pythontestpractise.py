066848e60d2fdb6f75dcbf47228eeee927924b95
maikszusevics <110176257+maikszusevics@users.noreply.github.com>
2022-08-10 11:02:20 +0100

    Add files via upload

---
+++ pythontestpractise.py
@@ -0,0 +1,95 @@
+# create a function called user_name
+
+# How would you iterate over the following list: arr = [1, 2, 3, 4, 5]
+#d isplay the list with help of for loop and a print statement
+
+
+# && and || andand which one is a correct boolean value/operator
+# && and || are boolean value operators
+
+# what is the diff between tuple and a list
+# tuple immutable
+
+# could you add an element to a tuple
+# no
+
+# can you have multiple types of data in a tuple
+# yes
+
+#data_type = {'0' : 0, '1' : 1, '2': 2} what type of data collection is this
+# dictionary
+
+# how to create an object of a class called DevOps():
+# maisk = DevOps()
+
+# adds 5 to the following list?
+# arr = [1,2,3,4]
+# arr.append(5)
+
+# return True if arg is equal to arg devops, ortherwise false
+# if arg == devops:
+      # return True
+# else:
+     # return False
+
+# how to inherit a class - write the correct syntax if you had class Y and class Z - inherit class z from class y
+
+# class z(y):
+
+
+# how to declare super
+# class Student(DevOps):
+#   def __init__(self, fname, lname):
+#     super().__init__(self, length, salary)
+
+#  a function should return True if value is contrained in [list] at an even index, false otherwise
+
+
+
+# create 4 functions that take 2 args each  percentage(), divide, calculate DOB, multiply
+# in each function check to ensure the value entered i snot 0
+# each function MUST RETURN correct mathemetical value
+
+def percentage(value1, value2):
+    return (value1/value2)*10
+
+def divide(value1, value2):
+    return value1/value2
+def multiply(value1, value2):
+    return value1*value2
+
+def calculate_DOB(day, month):
+    return f"{day}/{month}"
+
+
+
+# searching odd and even numbers using a function that takes an argument of ints
+def oddeven(x):
+    evens = []
+    odds = []
+    for i in range (x):
+        if i%2 == 0:
+            evens.append(i)
+        else:
+            odds.append(i)
+
+
+
+
+
+# create a function to calculate total value of shopping bill dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
